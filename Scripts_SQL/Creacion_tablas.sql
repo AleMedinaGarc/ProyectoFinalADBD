643eb3b4c1aa8fdb4f09f329b976db560d63c659
Eduardo Da Silva <61274696+EduardoSY@users.noreply.github.com>
2022-06-28 22:30:05 +0100

    Scripts trigger + crear tablas

---
+++ Scripts_SQL/Creacion_tablas.sql
@@ -0,0 +1,194 @@
+-- Script de creacion de las tablas de la base de datos
+-- Alejandro Medina Garcia
+-- Eduardo Da Silva Yanes
+
+-- Eliminar tablas
+DROP TABLE IF EXISTS BARCOS CASCADE;
+DROP TABLE IF EXISTS EMPRESA CASCADE;
+DROP TABLE IF EXISTS PATRON_CONTRATADO CASCADE;
+DROP TABLE IF EXISTS HORARIO CASCADE;
+DROP TABLE IF EXISTS MANTENIMIENTO CASCADE;
+DROP TABLE IF EXISTS USUARIOS_CLIENTES CASCADE;
+DROP TABLE IF EXISTS USUARIOS_ADMIN CASCADE;
+DROP TABLE IF EXISTS VERIFICADOS CASCADE;
+DROP TABLE IF EXISTS RESERVAS CASCADE;
+DROP TABLE IF EXISTS PAGO CASCADE;
+
+CREATE TYPE valid_turnos AS ENUM('tarde', 'mañana', 'todo');
+
+-- -----------------------------------------------------
+-- Table BARCOS
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS BARCOS (
+  MatriculaBarco INT NOT NULL,
+  Nombre VARCHAR(45) NOT NULL,
+  Puerto VARCHAR(45) NOT NULL,
+  Pantalan VARCHAR(45) NOT NULL,
+  Numero VARCHAR(45) NOT NULL,
+  Eslora INT NOT NULL,
+  PRIMARY KEY (MatriculaBarco));
+
+
+-- -----------------------------------------------------
+-- Table EMPRESA
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS EMPRESA (
+  NIFEmpresa VARCHAR(45) NOT NULL,
+  nombreEmpresa VARCHAR(45) NOT NULL,
+  Telefono INT NOT NULL,
+  Dirección VARCHAR(128) NULL,
+  PRIMARY KEY (NIFEmpresa));
+
+-- -----------------------------------------------------
+-- Table PATRON_CONTRATADO
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS PATRON_CONTRATADO (
+  DNI_patron VARCHAR(9) NOT NULL,
+  Nombre VARCHAR(45) NOT NULL,
+  Apellidos VARCHAR(70) NULL,
+  IBAN VARCHAR(45) NOT NULL,
+  Numero_Seguridad_Social VARCHAR(45) NOT NULL,
+  Direccion VARCHAR(45) NOT NULL,
+  Titulo_patron VARCHAR(3) NOT NULL,
+  PRIMARY KEY (DNI_patron));
+
+-- -----------------------------------------------------
+-- Table HORARIO
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS HORARIO (
+  idHorario SERIAL,
+  DNI_patron VARCHAR(9) NOT NULL,
+  dia_semana VARCHAR(45) NOT NULL,
+  Turno VALID_TURNOS NOT NULL,
+  PRIMARY KEY (idHorario),
+  CONSTRAINT DNI_patron
+    FOREIGN KEY (DNI_patron)
+    REFERENCES PATRON_CONTRATADO (DNI_patron)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
+
+
+-- -----------------------------------------------------
+-- Table MANTENIMIENTO
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS MANTENIMIENTO (
+  idMantenimiento SERIAL,
+  idBarco INT NOT NULL,
+  NIFEmpresa VARCHAR(45) NOT NULL,
+  Fecha DATE NOT NULL,
+  Turno VALID_TURNOS NOT NULL,
+  Precio DECIMAL(6) NOT NULL,
+  PRIMARY KEY (idMantenimiento),
+  CONSTRAINT idBarco
+    FOREIGN KEY (idBarco)
+    REFERENCES BARCOS (MatriculaBarco)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT NIFEmpresa
+    FOREIGN KEY (NIFEmpresa)
+    REFERENCES EMPRESA (NIFEmpresa)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
+
+-- -----------------------------------------------------
+-- Table USUARIOS_CLIENTES
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS USUARIOS_CLIENTES (
+  DNI_cliente VARCHAR(9) NOT NULL UNIQUE,
+  Correo VARCHAR(45) NOT NULL,
+  Nombre VARCHAR(35) NOT NULL,
+  Apellidos VARCHAR(65) NULL,
+  Password VARCHAR(45) NOT NULL,
+  Direccion VARCHAR(100) NOT NULL,
+  Titulo VARCHAR(3) NOT NULL,
+  Telefono INT NOT NULL,
+  PRIMARY KEY (DNI_cliente, Correo));
+
+-- -----------------------------------------------------
+-- Table USUARIOS_ADMIN
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS USUARIOS_ADMIN (
+  DNI_admin VARCHAR(9) NOT NULL UNIQUE,
+  Correo VARCHAR(45) NOT NULL,
+  Nombre VARCHAR(35) NOT NULL,
+  Apellidos VARCHAR(65) NULL,
+  Password VARCHAR(45) NOT NULL,
+  PRIMARY KEY (DNI_admin, Correo));
+
+
+-- -----------------------------------------------------
+-- Table VERIFICADOS
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS VERIFICADOS (
+  DNI_cliente VARCHAR(9) NOT NULL,
+  DNI_admin VARCHAR(9) NOT NULL,
+  Fecha VARCHAR(35) NOT NULL,
+  PRIMARY KEY (DNI_cliente),
+  CONSTRAINT DNI_cliente
+    FOREIGN KEY (DNI_cliente)
+    REFERENCES USUARIOS_CLIENTES (DNI_cliente)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT DNI_admin
+    FOREIGN KEY (DNI_admin)
+    REFERENCES USUARIOS_ADMIN (DNI_admin)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
+
+
+-- -----------------------------------------------------
+-- Table RESERVAS
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS RESERVAS (
+  idReserva SERIAL,
+  idBarco INT NOT NULL,
+  DNI_patron VARCHAR(9),
+  DNI_patron_cliente VARCHAR(9),
+  DNI_cliente VARCHAR(9) NOT NULL,
+  Fecha DATE NOT NULL,
+  Turno VALID_TURNOS NOT NULL,
+  Comentario VARCHAR(1000) NULL,
+  PRIMARY KEY (idReserva),
+  CONSTRAINT fk_DNI_cliente
+    FOREIGN KEY (DNI_cliente)
+    REFERENCES USUARIOS_CLIENTES (DNI_cliente)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT fk_ID_barco
+    FOREIGN KEY (idBarco)
+    REFERENCES BARCOS (MatriculaBarco)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT fk_DNI_patron_contratado
+    FOREIGN KEY (DNI_patron)
+    REFERENCES PATRON_CONTRATADO (DNI_patron)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT fk_DNI_patron_cliente
+    FOREIGN KEY (DNI_patron_cliente)
+    REFERENCES USUARIOS_CLIENTES (DNI_cliente)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT chk_only_one_patron CHECK (
+    (DNI_patron IS NOT NULL AND DNI_patron_cliente IS NULL) OR
+    (DNI_patron IS NULL AND DNI_patron_cliente IS NOT NULL)));
+
+-- -----------------------------------------------------
+-- Table PAGO
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS PAGO (
+  idPago SERIAL,
+  idReserva INT NOT NULL,
+  tipo_comprobante VARCHAR(45) NOT NULL,
+  num_comprobante INT NOT NULL,
+  total_pago DECIMAL(6) NOT NULL,
+  fecha_emision DATE NOT NULL,
+
+  fecha_pago DATE NOT NULL,
+  PRIMARY KEY (idPago),
+  CONSTRAINT idReserva
+    FOREIGN KEY (idReserva)
+    REFERENCES RESERVAS (idReserva)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
+
---
+++ Scripts_SQL/Triggers.sql
@@ -0,0 +1,137 @@
+---------------------------
+-- CREACION DE TRIGGERS
+-- Alejandro Medina Garcia
+-- Eduardo Da Silva Yanes
+---------------------------
+-------------------------------------
+-- PARTE 1: CREACION DE LAS FUNCIONES
+-------------------------------------
+-- TRIGGER FUNCTION 1
+DROP TRIGGER IF EXISTS check_mantenimiento_before_insert_reserva ON RESERVAS;
+DROP FUNCTION IF EXISTS check_mantenimiento();
+
+CREATE OR REPLACE FUNCTION check_mantenimiento() RETURNS TRIGGER AS $example$
+BEGIN
+  IF NEW.Fecha = ANY 
+    (SELECT DISTINCT  Fecha
+		 FROM MANTENIMIENTO
+     WHERE NEW.Fecha = Fecha AND NEW.idBarco = idBarco)
+  THEN
+    RAISE NOTICE 'Barco en mantenimiento';
+      RETURN NULL;
+  END IF;
+  RETURN NEW;
+END;
+$example$ LANGUAGE plpgsql;
+
+-- TRIGGER FUNCTION 2
+DROP TRIGGER IF EXISTS check_title_before_insert_reserva ON RESERVAS;
+DROP FUNCTION IF EXISTS check_title();
+
+CREATE OR REPLACE FUNCTION check_title() RETURNS TRIGGER AS $example2$
+DECLARE 
+  Eslora_R INTEGER;
+  Titulo_R VARCHAR;
+  Patron_R VARCHAR;	
+BEGIN
+	-- Buscar la eslora del barco perteneciente a la reserva
+	SELECT Eslora INTO Eslora_R
+	FROM BARCOS
+	WHERE NEW.idBarco = matriculaBarco;
+    
+    -- Buscar el titulo del cliente 
+IF NEW.DNI_patron = ANY
+  (SELECT DNI_patron FROM PATRON_CONTRATADO)
+THEN
+  SELECT Titulo_patron INTO Titulo_R
+	FROM PATRON_CONTRATADO
+	WHERE NEW.DNI_patron = DNI_patron;
+ELSE
+  SELECT Titulo INTO Titulo_R
+	FROM USUARIOS_CLIENTES
+	WHERE NEW.DNI_cliente = DNI_cliente;
+END IF;	
+
+
+    
+    -- Switch case de la eslora obvtenida segun el titulo
+	raise notice 'Value: %', Eslora_R;
+	CASE Eslora_R::INTEGER
+		WHEN 6::INTEGER THEN 
+			IF (Titulo_R::VARCHAR != 'LN' AND  Titulo_R::VARCHAR != 'PNB' AND Titulo_R::VARCHAR != 'PER' AND Titulo_R::VARCHAR != 'PY' AND Titulo_R::VARCHAR != 'CY') THEN 
+				RAISE NOTICE 'No puede navegar con un barco de eslora 6m';				
+				RETURN NULL;
+			END IF;
+		WHEN 8::INTEGER THEN
+			IF (Titulo_R::VARCHAR != 'PNB' AND Titulo_R::VARCHAR != 'PER' AND Titulo_R::VARCHAR != 'PY' AND Titulo_R::VARCHAR != 'CY') THEN 
+				RAISE NOTICE 'No puede navegar con un barco de eslora 8m';				
+				RETURN NULL;
+			END IF;
+		WHEN 15::INTEGER THEN
+			IF (Titulo_R::VARCHAR != 'PER' AND Titulo_R::VARCHAR != 'PY' AND Titulo_R::VARCHAR != 'CY') THEN 
+				RAISE NOTICE 'No puede navegar con un barco de eslora 15m';				
+				RETURN NULL;
+			END IF;
+		WHEN 24::INTEGER THEN
+			IF (Titulo_R::VARCHAR != 'PY' AND Titulo_R::VARCHAR != 'CY') THEN 
+				RAISE NOTICE 'No puede navegar con un barco de eslora 24m';				
+				RETURN NULL;
+			END IF;
+	END CASE;
+  RETURN NEW;
+END;
+$example2$ LANGUAGE plpgsql;
+
+-- TRIGGER FUNCTION 3
+DROP TRIGGER IF EXISTS check_verificado_before_insert_reserva ON RESERVAS;
+DROP FUNCTION IF EXISTS check_verificado();
+
+CREATE OR REPLACE FUNCTION check_verificado() RETURNS TRIGGER AS $example3$
+DECLARE 
+  User_R varchar;
+BEGIN
+	SELECT * INTO User_R
+    FROM VERIFICADOS 
+    WHERE DNI_CLIENTE = NEW.DNI_cliente;
+    
+	IF User_R = null  
+    THEN RETURN NULL;
+  END IF;
+ RETURN NEW;
+END;
+$example3$ LANGUAGE plpgsql;
+
+-- TRIGGER FUNCTION 4
+DROP TRIGGER IF EXISTS check_barco_ocupado_before_insert_reserva ON RESERVAS;
+DROP FUNCTION IF EXISTS check_barco_ocupado();
+
+CREATE OR REPLACE FUNCTION check_barco_ocupado() RETURNS TRIGGER AS $example4$
+DECLARE 
+  Boat_R varchar;
+BEGIN
+	SELECT * INTO Boat_R
+    FROM RESERVAS 
+    WHERE Fecha = NEW.Fecha AND Turno = NEW.Turno;
+    
+	IF Boat_R != NULL
+  THEN RETURN NULL;
+  END IF;
+
+  RETURN NEW;
+END;
+$example4$ LANGUAGE plpgsql;
+
+---------------------
+-- PARTE 2: CREACION DE LOS TRIGGERS
+---------------------
+CREATE TRIGGER check_mantenimiento_before_insert_reserva BEFORE INSERT ON RESERVAS
+FOR EACH ROW EXECUTE PROCEDURE check_mantenimiento();
+
+CREATE TRIGGER check_title_before_insert_reserva BEFORE INSERT ON RESERVAS
+FOR EACH ROW EXECUTE PROCEDURE check_title();
+
+CREATE TRIGGER check_verificado_before_insert_reserva BEFORE INSERT ON RESERVAS
+FOR EACH ROW EXECUTE PROCEDURE check_verificado();
+
+CREATE TRIGGER check_barco_ocupado_before_insert_reserva BEFORE INSERT ON RESERVAS
+FOR EACH ROW EXECUTE PROCEDURE check_barco_ocupado();
